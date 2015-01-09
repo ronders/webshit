949a3b241ea55a0fb5dc9bf6084d736fb4e63127
Ron Dyrcz <ron@port80corp.com>
2015-01-09 14:37:12 -0600

    Install of OpenCart 5.6.4

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/admin/');
+define('HTTP_CATALOG', 'http://localhost:8888/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8888/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/Ronders/Projects/webshit/admin/');
+define('DIR_SYSTEM', '/Users/Ronders/Projects/webshit/system/');
+define('DIR_DATABASE', '/Users/Ronders/Projects/webshit/system/database/');
+define('DIR_LANGUAGE', '/Users/Ronders/Projects/webshit/admin/language/');
+define('DIR_TEMPLATE', '/Users/Ronders/Projects/webshit/admin/view/template/');
+define('DIR_CONFIG', '/Users/Ronders/Projects/webshit/system/config/');
+define('DIR_IMAGE', '/Users/Ronders/Projects/webshit/image/');
+define('DIR_CACHE', '/Users/Ronders/Projects/webshit/system/cache/');
+define('DIR_DOWNLOAD', '/Users/Ronders/Projects/webshit/download/');
+define('DIR_LOGS', '/Users/Ronders/Projects/webshit/system/logs/');
+define('DIR_CATALOG', '/Users/Ronders/Projects/webshit/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'mysql');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/Ronders/Projects/webshit/catalog/');
+define('DIR_SYSTEM', '/Users/Ronders/Projects/webshit/system/');
+define('DIR_DATABASE', '/Users/Ronders/Projects/webshit/system/database/');
+define('DIR_LANGUAGE', '/Users/Ronders/Projects/webshit/catalog/language/');
+define('DIR_TEMPLATE', '/Users/Ronders/Projects/webshit/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/Ronders/Projects/webshit/system/config/');
+define('DIR_IMAGE', '/Users/Ronders/Projects/webshit/image/');
+define('DIR_CACHE', '/Users/Ronders/Projects/webshit/system/cache/');
+define('DIR_DOWNLOAD', '/Users/Ronders/Projects/webshit/download/');
+define('DIR_LOGS', '/Users/Ronders/Projects/webshit/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'mysql');
+define('DB_PREFIX', 'oc_');
+?>
